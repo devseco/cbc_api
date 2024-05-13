6a2cb14dfb313f11173de183d2fd9f86ff915afe
Asecq <sec.alsha1b@gmail.com>
2024-05-13 21:36:09 +0300

    config

--- CBC/config/db.js
+++ CBC/config/db.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql');
 
 const db = mysql.createPool({
- host:"localhost",
- user:'root',
- password:'',
- database:'cbcDB'
+  host: "185.166.188.103",
+  database: 'u594579701_cbc',
+  password: '@Asec1997@',
+  user: 'u594579701_cbcroot'
 });
-db.getConnection(()=>{
-console.log("Connect Db is Successfully ..");
+// الاتصال بقاعدة البيانات والتعامل مع الأخطاء
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("خطأ في الاتصال بقاعدة البيانات:", err);
+    return;
+  }
+  console.log("Connect Db is Successfully ..");
 });
 module.exports = db;

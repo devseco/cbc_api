b21312d3e6ff773d55e9c5e35fe872230303aded
Asecq <sec.alsha1b@gmail.com>
2024-06-17 05:00:41 +0300

    add Page Product AQS

--- AQS/models/Product.js
+++ AQS/models/Product.js
@@ -31,15 +31,37 @@ class Product {
     });
 }
 
-    static async getProduct(id){
-        return new Promise(resolve=>{
-            mysql.query('select * from products where id = ?' , [id], (error , result)=>{
-                if(!error){
-                    resolve(result);
+static async getProduct(id) {
+    return new Promise((resolve, reject) => {
+        const productQuery = 'SELECT * FROM products WHERE id = ?';
+
+        mysql.query(productQuery, [id], (productError, productResult) => {
+            if (productError) {
+                return reject(productError);
+            }
+
+            if (productResult.length === 0) {
+                return resolve(null); // Return null if no product is found
+            }
+
+            const product = productResult[0];
+
+            const imagesQuery = 'SELECT image FROM images_product WHERE productId = ?';
+            mysql.query(imagesQuery, [id], (imagesError, imagesResult) => {
+                if (imagesError) {
+                    return reject(imagesError);
                 }
+
+                const images = imagesResult.map(row => row.image);
+                product.images = images;
+
+                resolve(product);
             });
-        })
-    }
+        });
+    });
+}
+
+
     static async getProductByCategory(id){
         return new Promise(resolve=>{
             mysql.query('select * from products where category = ?' , [id], (error , result)=>{
--- index.js
+++ index.js
@@ -13,11 +13,6 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-const sslOptions = {
-  key: fs.readFileSync('./CBC/cetificates/key.pem'),
-  cert: fs.readFileSync('./CBC/cetificates/cert.pem'), 
-  ca: fs.readFileSync('./CBC/cetificates/89_116_110_51.ca-bundle'), 
-};
 
 const networkInterfaces = os.networkInterfaces();
 
@@ -39,9 +34,9 @@ app.use('/aqs/api/v1/', router_aqa);
 app.use('/uploads', express.static('uploads'));
 
 // إنشاء خادم HTTPS
-https.createServer(sslOptions, app).listen(port, () => {
-  console.log(`Running with ip ${ip}:${port}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
